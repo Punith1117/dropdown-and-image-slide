6d7fa107cecb2af13daa2dad6bf7f5dc7e59f4e4
Punith1117 <punith11172006@gmail.com>
2024-10-20 20:41:03 +0530

    make index.js prettier

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
 import "./reset.css";
 import "./style.css";
 
-let dropdownName = document.querySelector('.dropdown-name');
-let dropDownOptions = document.querySelectorAll('.option');
+let dropdownName = document.querySelector(".dropdown-name");
+let dropDownOptions = document.querySelectorAll(".option");
 
-dropdownName.addEventListener('click', () => {
-    let dropdownContent = document.querySelector('.dropdown-content');
-    if(dropdownContent.classList.contains('displaying')) {
-        dropdownName.textContent = 'Dropdown \u2191';
-        dropdownContent.classList.remove('displaying');
-    } else {
-        dropdownName.textContent = 'Dropdown \u2193';
-        displayDropdownContent();
-    }
-})
+dropdownName.addEventListener("click", () => {
+  let dropdownContent = document.querySelector(".dropdown-content");
+  if (dropdownContent.classList.contains("displaying")) {
+    dropdownName.textContent = "Dropdown \u2191";
+    dropdownContent.classList.remove("displaying");
+  } else {
+    dropdownName.textContent = "Dropdown \u2193";
+    displayDropdownContent();
+  }
+});
 
 dropDownOptions.forEach((option, index) => {
-    option.addEventListener('click', () => {
-        let dropdownSection = document.querySelector('.dropdown-section');
-        let bgColor = getColor(index);
-        dropdownSection.style.backgroundColor = bgColor; 
-    })
-})
+  option.addEventListener("click", () => {
+    let dropdownSection = document.querySelector(".dropdown-section");
+    let bgColor = getColor(index);
+    dropdownSection.style.backgroundColor = bgColor;
+  });
+});
 
 function displayDropdownContent() {
-    let dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.classList.add('displaying');
+  let dropdownContent = document.querySelector(".dropdown-content");
+  dropdownContent.classList.add("displaying");
 }
 
 function getColor(index) {
-    switch(index) {
-        case 0:
-            return 'lightblue';
-        case 1:
-            return '#ff6f6f';
-        case 2:
-            return 'lightgreen';
-        case 3:
-            return 'grey';
-    }
+  switch (index) {
+    case 0:
+      return "lightblue";
+    case 1:
+      return "#ff6f6f";
+    case 2:
+      return "lightgreen";
+    case 3:
+      return "grey";
+  }
 }
